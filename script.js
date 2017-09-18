f3624bbc6df2515de7e7cf330bf6102f966901aa
pcmkv <maxwell1111@mail.ru>
2017-09-18 23:10:52 +0300

    6 задание не совсем понятное

---
+++ script.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+    var myArray = document.getElementById('new');
+    inititArray(myArray);
+};
+var array = new Array(10);
+
+function inititArray(elem) {
+    for (var i = 0; i < array.length; i++) {
+        array[i] = Math.floor((Math.random() * 10) + 1);
+        elem.innerHTML  = elem.innerHTML+array[i]+"</br>";
+    }
+}
+
+function addItem(number,indexEl,elem){
+	array.splice(indexEl,0,number);
+	document.getElementById("number").value = "";
+	document.getElementById("index").value ="";
+	elem.innerHTML ='';
+	for (var i = 0; i < array.length; i++) {
+	    elem.innerHTML  = elem.innerHTML+array[i]+"</br>";
+	}
+	console.log("number "+number+" index "+indexEl);
+}
+
+function popMethod() {
+    var lastElem = array.pop();
+    alert("Последний е-т:"+lastElem);
+}
+
+function shiftMethod() {
+    var firstElem = array.shift();
+    alert("Первый е-т: "+firstElem);
+}
+
+function pushMethod(elem){
+	var numberVal = document.getElementById("number").value;
+	var indexElem = document.getElementById("index").value;
+    array.splice(indexElem, 0, numberVal);
+    document.getElementById("number").value = "";
+    document.getElementById("index").value ="";
+     elem.innerHTML ='';
+     for (var i = 0; i < array.length; i++) {
+        elem.innerHTML  = elem.innerHTML+array[i]+"</br>";
+    }
+    indexElem  = document.getElementById("index").value;
+    console.log("numberVal",array);
+    alert(array);
+}
+
+/*
+function deleteMethod(elem){
+	var numberVal = document.getElementById("number").value;
+	var indexElem = document.getElementById("index").value;
+    //array.push(parseInt(numberVal));
+    array.splice(indexElem, 1, numberVal);
+    document.getElementById("number").value = "";
+    document.getElementById("index").value ="";
+    elem.innerHTML ='';
+    for (var i = 0; i < array.length; i++) {
+        elem.innerHTML  = elem.innerHTML+array[i]+"</br>";
+    }
+    indexElem  = document.getElementById("index").value;
+    console.log("numberVal",array);
+    alert(array);
+}
+*/
+
